60fbb50c90376c7aea0f062b216734a4da4c63af
antonchekantsev <kl4ym4n@gmail.com>
2025-02-14 14:13:20 +0700

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,24 @@ export default defineConfig({
     port: 3000,
     open: true
   },
-  base: "/",
+  base: "/sindbad-boardgame/",
   build: {
     assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (/\.(png|jpe?g|svg|gif|webp)$/.test(assetInfo.name)) {
+            return "assets/images/[name]-[hash][extname]"; // 👈 Переносит картинки в `assets/images/`
+          }
+          return "assets/[name]-[hash][extname]"; // 👈 Остальные файлы идут в `assets/`
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "/assets/images/": "/sindbad-boardgame/assets/images/",
+      "/shaders/": "/sindbad-boardgame/shaders/",
+    },
   },
 }); 
